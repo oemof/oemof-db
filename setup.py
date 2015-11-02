2022d618fd68894c92df7968d5e3ae4a8436ca88
uwe <uwe.krien@rl-institut.de>
2015-11-02 10:26:47 +0100

    add sqlalchemy and keyring to the requirement list of the setup file

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@
 
 from distutils.core import setup
 
-setup( name='oemof-pg'
-     , version='0.0.1dev'
-     , description='The oemof postgis extension'
-     , package_dir = {'oemof_pg': 'oemof_pg'}
-     )
-
+setup(name='oemof-pg',
+      version='0.0.1dev',
+      description='The oemof postgis extension',
+      package_dir={'oemof_pg': 'oemof_pg'},
+      install_requires=['sqlalchemy >= 1.0',
+                        'keyring >= 4.0'])

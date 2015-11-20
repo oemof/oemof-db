bfb19bab53060e059161e7cacc3996826a504d07
uwe <uwe.krien@rl-institut.de>
2015-11-20 23:04:30 +0100

    add feedin calculation using coastdat2 and EnergyMap

--- oemof_pg/feedin_pg.py
+++ oemof_pg/feedin_pg.py
@@ -2,90 +2,122 @@
 # -*- coding: utf-8 -*-
 
 import logging
+import pandas as pd
+import os.path as path
 
-def fetch_ee_feedin(self, conn, **site):
-    site['connection'] = conn
-    pv_df = 0
-    wind_df = 0
-    if self.power_plants.get('re', None) is None:
-        self.power_plants['re'] = (
-            pp.Power_Plants().get_empty_power_plant_df())
+from . import coastdat
+from . import powerplants as pg_pp
+from . import tools
+from feedinlib import powerplants as pp
+from oemof.core.network.entities.components import sources as source
 
-    # Define height dict
-    data_height = {}
-    for key in self.weather.datatypes:
-        name = self.weather.name_dc[key]
-        data_height[name] = self.weather.get_data_heigth(name)
-        if data_height[name] is None:
-            data_height[name] = 0
 
-    laenge = len(self.weather.gid)
+class Feedin:
+    ''
 
-    for gid in self.weather.gid:
-        logging.debug(laenge)
-        # Get the geometry for the given weather raster field
-        tmp_geom = self.weather.get_geometry_from_gid(gid)
+    def __init__(self):
+        ''
+        pass
 
-        # Get all Power Plants for raster field
-        ee_pp = pp.Power_Plants().get_all_re_power_plants(
-            conn, tmp_geom, self.geometry)
+    def create_fixed_source(self, conn, **kwargs):
+        ''
+        region = kwargs['region']
+        [pv_df, wind_df, cap] = self.get_timeseries(
+            conn,
+            geometry=region.geom,
+            year=region.year,
+            **kwargs)
 
-        # Add the powerplants to the power plant table of the region
-        self.power_plants['re'] = pd.concat(
-            [ee_pp, self.power_plants['re']], ignore_index=True)
+        if kwargs.get('store', False):
+            self.store_full_df(pv_df, wind_df, **kwargs)
 
-        # Find type of wind turbine and its parameters according to the
-        # windzone.
-        wz = pg_helpers.get_windzone(conn, tmp_geom)
-        site['wind_conv_type'] = (site['wka_model_dc'].get(
-            wz, site['wka_model']))
-        site['d_rotor'] = (site['d_rotor_dc'].get(wz, site['d_rotor']))
-        site['h_hub'] = (site['h_hub_dc'].get(wz, site['h_hub']))
+        # Summerize the results to one column for pv and one for wind
+        df = pd.concat([pv_df.sum(axis=1), wind_df.sum(axis=1)], axis=1)
+        feedin_df = df.rename(columns={0: 'pv_pwr', 1: 'wind_pwr'})
 
-        # Define weather object of the feedinlib
-        self.weather.set_feedin_dataset(gid)
+        source_list = []
 
-        # Determine the feedin time series for the weather field
-        # Wind energy
-        wind_peak_power = ee_pp[ee_pp.type == 'Windkraft'].p_kw_peak.sum()
-        wind_power_plant = plants.WindPowerPlant(**site)
-        wind_series = wind_power_plant.feedin(
-            weather=self.weather,
-            installed_capacity=wind_peak_power)
-        wind_series.name = gid
+        for stype in feedin_df.keys():
+            source_list.append(source.FixedSource(
+                uid='_'.join(['DispSrc', region.code, stype]),
+                outputs=[region.buses['_'.join(['b', region.code, 'el'])]],
+                val=feedin_df[stype],
+                out_max={'_'.join(['b', region.code, 'el']): float(
+                    cap.sum()[stype])}))
+        return source_list
 
-        # PV
-        pv_peak_power = ee_pp[ee_pp.type == 'Solarstrom'].p_kw_peak.sum()
-        pv_plant = plants.Photovoltaic(**site)
-        pv_series = pv_plant.feedin(
-            weather=self.weather, peak_power=pv_peak_power)
-        pv_series.name = gid
+    def get_timeseries(self, conn, **kwargs):
+        ''
+        weather = coastdat.get_weather(
+            conn, kwargs['geometry'], kwargs['year'])
 
-        # Combine the results to a DataFrame
-        try:
-            pv_df = pd.concat([pv_df, pv_series], axis=1)
-            wind_df = pd.concat([wind_df, wind_series], axis=1)
-        except:
-            pv_df = pv_series.to_frame()
-            wind_df = wind_series.to_frame()
+        pv_df = 0
+        pv_cap = {}
+        wind_df = 0
+        wind_cap = {}
 
-        laenge -= 1
+        if not isinstance(weather, list):
+            weather = [weather]
 
-    if site.get('store'):
-        dpath = site.get(
+        for w_cell in weather:
+            ee_pps = pg_pp.get_energymap_pps(
+                conn, geometry1=w_cell.geometry, geometry2=kwargs['geometry'])
+
+            # Find type of wind turbine and its parameters according to the
+            # windzone.
+            wz = tools.get_windzone(conn, w_cell.geometry)
+
+            kwargs['wind_conv_type'] = (kwargs['wka_model_dc'].get(
+                wz, kwargs['wka_model']))
+            kwargs['d_rotor'] = (kwargs['d_rotor_dc'].get(
+                wz, kwargs['d_rotor']))
+            kwargs['h_hub'] = (kwargs['h_hub_dc'].get(wz, kwargs['h_hub']))
+
+            # Determine the feedin time series for the weather cell
+            # Wind energy
+            wind_peak_power = ee_pps[ee_pps.type == 'wind_power'].cap.sum()
+            wind_power_plant = pp.WindPowerPlant(**kwargs)
+            wind_series = wind_power_plant.feedin(
+                weather=w_cell, installed_capacity=wind_peak_power)
+            wind_series.name = w_cell.name
+            wind_cap[w_cell.name] = wind_peak_power
+
+            # PV
+            pv_peak_power = ee_pps[ee_pps.type == 'solar_power'].cap.sum()
+            pv_plant = pp.Photovoltaic(**kwargs)
+            pv_series = pv_plant.feedin(
+                weather=w_cell, peak_power=pv_peak_power)
+            pv_series.name = w_cell.name
+            pv_cap[w_cell.name] = pv_peak_power
+
+            # Combine the results to a DataFrame
+            try:
+                pv_df = pd.concat([pv_df, pv_series], axis=1)
+                wind_df = pd.concat([wind_df, wind_series], axis=1)
+            except:
+                pv_df = pv_series.to_frame()
+                wind_df = wind_series.to_frame()
+
+        # Write capacity into a dataframe
+        capw = pd.Series(pd.DataFrame.from_dict(wind_cap, orient='index')[0])
+        capw.name = 'wind_pwr'
+        cappv = pd.Series(pd.DataFrame.from_dict(pv_cap, orient='index')[0])
+        cappv.name = 'pv_pwr'
+        cap = pd.concat([capw, cappv], axis=1)
+
+        return pv_df, wind_df, cap
+
+    def store_full_df(self, pv_df, wind_df, **kwargs):
+        ''
+        dpath = kwargs.get(
             'dpath', path.join(path.expanduser("~"), '.oemof'))
-        filename = site.get('filename', self.name)
+        filename = kwargs.get('filename', 'feedin_' + kwargs['region'].name)
         fullpath = path.join(dpath, filename)
 
-        if site['store'] == 'hf5':
+        if kwargs['store'] == 'hf5':
             pv_df.to_hdf(fullpath + '.hf5', 'pv_pwr')
             wind_df.to_hdf(fullpath + '.hf5', 'wind_pwr')
 
-        if site['store'] == 'csv':
+        if kwargs['store'] == 'csv':
             pv_df.to_csv(fullpath + '_pv.csv')
             wind_df.to_csv(fullpath + '_wind.csv')
-
-    # Summerize the results to one column for pv and one for wind
-    df = pd.concat([pv_df.sum(axis=1), wind_df.sum(axis=1)], axis=1)
-    self.feedin = df.rename(columns={0: 'pv_pwr', 1: 'wind_pwr'})
-    return self

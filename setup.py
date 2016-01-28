132ba8c4874e67b3a9bfd8b8a0f145f5d41be5b9
Stephan Günther <gnn.code@gmail.com>
2016-01-28 19:52:03 +0100

    Explicitly state contained packages
    ...via `setuptools`'s `find_packages` method. Without it, I don't even
    think packaging should work. In order for `find_packages` to register
    `oemof` as a python package, the `oemof` folder has to contain an
    `__init__.py` file.

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 #! /usr/bin/env python
 
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(name='oemof.db',
       version='0.0.1dev',
       description='The oemof database extension',
       namespace_package = ['oemof'],
+      packages=find_packages(),
       package_dir={'oemof': 'oemof'},
       install_requires=['sqlalchemy >= 1.0',
                         'keyring >= 4.0'])

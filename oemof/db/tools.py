9a2f4f4101bedfcda7b69d5f14d4ab0a2362fd44
Guido <guido.plessmann@rl-institut.de>
2016-03-14 18:24:32 +0100

    grant database rights

--- oemof/db/tools.py
+++ oemof/db/tools.py
@@ -324,3 +324,25 @@ def create_empty_table_serial_primary(conn, schema, table, columns=None, id_col=
             col_str = """alter table {schema}.{table} add column {col} double precision;
                 """.format(schema=schema, table=table, col=col)
             conn.execute(col_str)
+
+
+def grant_db_access(conn, schema, table, role):
+    r"""Gives access to database users/ groups
+
+
+    Parameters
+    ----------
+    conn : sqlalchemy connection object
+        A valid connection to a database
+    schema : str
+        The database schema
+    table : str
+        The database table
+    role : str
+        database role that access is granted to
+
+    """
+    grant_str = """GRANT ALL ON TABLE {schema}.{table}
+    TO {role} WITH GRANT OPTION;""".format(schema=schema, table=table, role=role)
+
+    conn.execute(grant_str)

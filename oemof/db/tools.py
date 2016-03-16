68f4d976d530d321f9af204e29c7dbf76df75124
Guido <guido.plessmann@rl-institut.de>
2016-03-16 13:58:26 +0100

    Add primary key constraint setter definition

--- oemof/db/tools.py
+++ oemof/db/tools.py
@@ -349,3 +349,23 @@ def grant_db_access(conn, schema, table, role):
 
     conn.execute(grant_str)
     conn.execute(grant_str)
+
+def add_primary_key(conn, schema, table, pk_col):
+    r"""Adds primary key to database table
+
+    Parameters
+    ----------
+    conn : sqlalchemy connection object
+        A valid connection to a database
+    schema : str
+        The database schema
+    table : str
+        The database table
+    pk_col : str
+        Column that primary key is applied to
+
+    """
+    sql_str = """alter table {schema}.{table} add primary key ({col})""".format(
+        schema=schema, table=table, col=pk_col)
+
+    conn.execute(sql_str)

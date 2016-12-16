cf8b7a0a1ec1f643778d4254a8c587962f313741
gplessm <guido.plessmann@rl-institut.de>
2016-12-16 18:58:47 +0100

    Update version number and add new whatsnew file

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import find_packages, setup
 
 setup(name='oemof.db',
-      version='0.0.4',
+      version='0.0.5dev',
       description='The oemof database extension',
       namespace_package = ['oemof'],
       packages=find_packages(),

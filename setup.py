2752ac232b89bf80a203f09ac93317e190e31341
Stephan Günther <gnn.code@gmail.com>
2019-12-01 22:11:07 +0100

    Blacken `setup.py`
    Done via `black -l 79 -S setup.py`.

--- setup.py
+++ setup.py
@@ -2,18 +2,22 @@
 
 from setuptools import find_packages, setup
 
-setup(name='oemof.db',
-      # Unfortunately we can't use a `__version__` attribute on `oemof.db` as
-      # we can't import that module here. It depends on packages which might
-      # not be available prior to installation.
-      version='0.0.6dev',
-      description='The oemof database extension',
-      namespace_package = ['oemof'],
-      packages=find_packages(),
-      package_dir={'oemof': 'oemof'},
-      install_requires=['sqlalchemy >= 1.0',
-                        'keyring >= 4.0',
-                        'shapely',
-                        'psycopg2',
-                        'keyrings.alt',
-                        'pandas >=0.19.1'])
+setup(
+    name='oemof.db',
+    # Unfortunately we can't use a `__version__` attribute on `oemof.db` as
+    # we can't import that module here. It depends on packages which might
+    # not be available prior to installation.
+    version='0.0.6dev',
+    description='The oemof database extension',
+    namespace_package=['oemof'],
+    packages=find_packages(),
+    package_dir={'oemof': 'oemof'},
+    install_requires=[
+        'sqlalchemy >= 1.0',
+        'keyring >= 4.0',
+        'shapely',
+        'psycopg2',
+        'keyrings.alt',
+        'pandas >=0.19.1',
+    ],
+)
